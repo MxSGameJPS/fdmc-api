203a24a63e49e9c9fd37b13bdd726aa6726a6c36
MxSGameJPS <mxsgamejps@gmail.com>
2025-07-26 15:43:18 -0300

    refactor: especificar campos retornados nas consultas de jogos e elenco

--- src/app/api/jogos/route.ts
+++ src/app/api/jogos/route.ts
@@ -4,19 +4,19 @@ import { supabase } from "@/lib/supabaseClient";
 export async function GET() {
   const { data: jogos, error: jogosError } = await supabase
     .from("jogos")
-    .select("*")
+    .select(
+      "id, mandante, visitante, resultado_mandante, resultado_visitante, gols_marcados, melhor_jogador, pior_jogador, data, hora, campeonato, rodada, local, escudo_mandante, escudo_visitante"
+    )
     .order("data", { ascending: true });
 
   const { data: elenco, error: elencoError } = await supabase
     .from("elenco")
-    .select("*")
-    .order("nome", { ascending: true });
+    .select("id, nome, posicao")
+    .order("id", { ascending: true });
 
   if (jogosError || elencoError) {
     return NextResponse.json(
-      {
-        error: jogosError?.message || elencoError?.message,
-      },
+      { error: jogosError?.message || elencoError?.message },
       { status: 500 }
     );
   }
